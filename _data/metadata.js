d629b9aafee09206ed6b9b549efb2111d19e43de
newick <erick@entre-quote.com>
2024-11-12 14:20:13 +0100

    met à jour le bandeau follow us

--- _data/metadata.js
+++ _data/metadata.js
@@ -10,5 +10,8 @@ module.exports = {
         email: "contact@nom.du.service.gouv.fr",
         url: "https://url_du_service.gouv.fr/"
     },
-    service: "Nom de l’entité (ministère, secrétariat d‘état, gouvernement)"
+    service: "Nom de l’entité (ministère, secrétariat d‘état, gouvernement)",
+		linkedin_url:"https://www.linkedin.com/company/mission-interministerielle-numerique-ecoresponsable",
+		twitter_url:"https://twitter.com/Mi_NumEco_Gouv",
+		instagram_url:"https://www.instagram.com/minumecogouv/"
 }
