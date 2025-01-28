12daec392a9d7ad491f0a3afa2e82a01a481be7c
newick <erick@entre-quote.com>
2025-01-28 09:50:35 +0100

    remet le nom de la DINUM

--- _data/data.js
+++ _data/data.js
@@ -1,5 +1,5 @@
 module.exports = {
-    footer_content_description: `La mission interministérielle numérique écoresponsable pilotée par la <a href="https://www.numerique.gouv.fr/"><abbr title="Direction Interministérielle du Numérique">DINUM</abbr></a> et le <a href="https://www.ecologie.gouv.fr/">Ministère de la Transition écologique</a>.`,
+    footer_content_description: `La mission interministérielle numérique écoresponsable pilotée par la <a href="https://www.numerique.gouv.fr/">Direction Interministérielle du Numérique</a> et le <a href="https://www.ecologie.gouv.fr/">Ministère de la Transition écologique</a>.`,
 
 		defaultLang: "fr"
 }
